4308c853cab601c5260ce042cbb1b143f9b7a371
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-01-05 18:59:11 -0300

    FESTOU-129: Ajuste no carregamento do refresh: Estado inicial + uso do estado anterior no "loading"

--- lib/src/features/show spaces/surrounding spaces/surrounding_spaces_page.dart
+++ lib/src/features/show spaces/surrounding spaces/surrounding_spaces_page.dart
@@ -21,9 +21,11 @@ class SurroundingSpacesPage extends ConsumerStatefulWidget {
 //TODO: spaces by surrounding area
 
 class _SurroundingSpacesPageState extends ConsumerState<SurroundingSpacesPage> {
+  SurroundingSpacesState previousState = SurroundingSpacesState(
+      status: SurroundingSpacesStateStatus.loaded, spaces: []);
   GoogleMapController? mapController;
   CameraPosition? currentCameraPosition;
-  bool canRefresh = false;
+  var canRefresh = false;
 
   LatLngBounds rioDeJaneiroBounds = LatLngBounds(
     southwest: const LatLng(-23.336615865621933, -43.460534401237965),
@@ -46,56 +48,87 @@ class _SurroundingSpacesPageState extends ConsumerState<SurroundingSpacesPage> {
     return Scaffold(
       body: allSpaces.when(
         data: (SurroundingSpacesState data) {
-          return Stack(
-            children: [
-              GoogleMap(
-                onMapCreated: (controller) {
-                  mapController = controller;
-                },
-                initialCameraPosition: currentCameraPosition ??
-                    const CameraPosition(
-                      target: LatLng(-22.9122, -43.2303),
-                      zoom: 10,
-                    ),
-                onCameraMove: (CameraPosition cameraPosition) async {
-                  currentCameraPosition = cameraPosition;
-                  // Esta função será chamada sempre que a câmera do mapa for movida
+          // Atualiza o estado anterior quando os dados são carregados
+          previousState = data;
+          return buildMap(data);
+        },
+        error: (Object error, StackTrace stackTrace) {
+          return const Stack(children: [
+            Center(child: Text('Inserir imagem melhor papai')),
+            Center(child: Icon(Icons.error)),
+          ]);
+        },
+        loading: () {
+          // Usa o estado anterior durante o carregamento
+          return buildMap(previousState);
+        },
+      ),
+    );
+  }
 
-                  // Agora você pode usar a visibleRegion para obter os limites visíveis do mapa
-                  // e realizar qualquer lógica adicional, como chamar o Firestore para recuperar espaços dentro desses limites.
-                },
-                circles: {
-                  ...data.spaces.map((space) {
-                    return Circle(
-                      circleId: CircleId(space.spaceId.toString()),
-                      center: LatLng(space.latitude, space.longitude),
-                      radius: 1000,
-                      strokeWidth: 0,
-                      fillColor: Colors.red.withOpacity(0.3),
-                      strokeColor: Colors.red,
-                    );
-                  }),
-                },
-                markers: data.spaces.map((space) {
-                  return Marker(
-                    onTap: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => NewCardInfo(space: space),
-                      ),
-                    ),
-                    markerId: MarkerId(space.spaceId),
-                    position: LatLng(space.latitude, space.longitude),
-                    // Adicione outras informações sobre o espaço, se necessário
-                    // infoWindow: InfoWindow(title: space.name, snippet: space.description),
-                  );
-                }).toSet(),
+  // Extrai o código do mapa para um método separado
+  Widget buildMap(SurroundingSpacesState data) {
+    return Stack(
+      alignment: Alignment.topCenter,
+      children: [
+        GoogleMap(
+          onCameraMoveStarted: () {
+            if (canRefresh) {
+              return;
+            } else {
+              setState(() {
+                canRefresh = true;
+              });
+            }
+
+            log(canRefresh.toString());
+          },
+          onMapCreated: (controller) {
+            mapController = controller;
+          },
+          initialCameraPosition: currentCameraPosition ??
+              const CameraPosition(
+                target: LatLng(-22.9122, -43.2303),
+                zoom: 10,
               ),
-              Positioned(
-                top: 16.0,
-                right: 16.0,
-                child: FloatingActionButton(
-                  onPressed: () async {
+          onCameraMove: (CameraPosition cameraPosition) async {
+            currentCameraPosition = cameraPosition;
+            // Esta função será chamada sempre que a câmera do mapa for movida
+
+            // Agora você pode usar a visibleRegion para obter os limites visíveis do mapa
+            // e realizar qualquer lógica adicional, como chamar o Firestore para recuperar espaços dentro desses limites.
+          },
+          circles: {
+            ...data.spaces.map((space) {
+              return Circle(
+                circleId: CircleId(space.spaceId.toString()),
+                center: LatLng(space.latitude, space.longitude),
+                radius: 1000,
+                strokeWidth: 0,
+                fillColor: Colors.red.withOpacity(0.3),
+                strokeColor: Colors.red,
+              );
+            }),
+          },
+          markers: data.spaces.map((space) {
+            return Marker(
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => NewCardInfo(space: space),
+                ),
+              ),
+              markerId: MarkerId(space.spaceId),
+              position: LatLng(space.latitude, space.longitude),
+            );
+          }).toSet(),
+        ),
+        Positioned(
+          width: 230,
+          top: 30,
+          child: ElevatedButton(
+            onPressed: canRefresh != false
+                ? () async {
                     LatLngBounds visibleRegion =
                         await mapController!.getVisibleRegion();
                     /*eu estava em duvida em sincronizar o initialCameraPosition
@@ -107,26 +140,13 @@ class _SurroundingSpacesPageState extends ConsumerState<SurroundingSpacesPage> {
                     setState(() {
                       rioDeJaneiroBounds = visibleRegion;
                     });
-                  },
-                  child: const Icon(Icons.refresh),
-                ),
-              ),
-            ],
-          );
-        },
-        error: (Object error, StackTrace stackTrace) {
-          return const Stack(children: [
-            Center(child: Text('Inserir imagem melhor papai')),
-            Center(child: Icon(Icons.error)),
-          ]);
-        },
-        loading: () {
-          return const Stack(children: [
-            Center(child: Text('Inserir carregamento Personalizado papai')),
-            Center(child: CircularProgressIndicator()),
-          ]);
-        },
-      ),
+                    canRefresh = false;
+                  }
+                : null,
+            child: const Text('Carregar espaços nessa área'),
+          ),
+        ),
+      ],
     );
   }
 }
