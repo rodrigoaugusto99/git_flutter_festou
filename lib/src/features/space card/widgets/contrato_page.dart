69af5c3ced494699918ca5101c6934299ea11c82
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-05-31 01:36:18 -0300

    add: preparacao pra arrumar HTML

--- lib/src/features/space card/widgets/contrato_page.dart
+++ lib/src/features/space card/widgets/contrato_page.dart
@@ -14,12 +14,14 @@ class ContratoPage extends StatefulWidget {
   final SpaceModel spaceModel;
   final int checkInTime;
   final int checkOutTime;
+  final String html;
   const ContratoPage({
     super.key,
     required this.spaceModel,
     required this.selectedDate,
     required this.checkInTime,
     required this.checkOutTime,
+    required this.html,
   });
 
   @override
@@ -35,7 +37,8 @@ class _ContratoPageState extends State<ContratoPage> {
     String modifiedHtml = html;
 
 //todo: replace all markers
-    // modifiedHtml = modifiedHtml.replaceAll('{name}', '<b>$name</b>');
+    modifiedHtml = modifiedHtml.replaceAll(
+        '{Data de Início do Evento}', '<b>${widget.checkInTime}</b>');
     // modifiedHtml = modifiedHtml.replaceAll('{name}', '<b>$name</b>');
     // modifiedHtml = modifiedHtml.replaceAll('{name}', '<b>$name</b>');
     // modifiedHtml = modifiedHtml.replaceAll('{name}', '<b>$name</b>');
@@ -109,8 +112,7 @@ class _ContratoPageState extends State<ContratoPage> {
         decoration: const BoxDecoration(),
         child: SingleChildScrollView(
           child: Html(
-            data:
-                '<h1>CONTRATO DE LOCAÇÃO DE ESPAÇO PARA EVENTOS - FESTOU</h1><h2>IDENTIFICAÇÃO DAS PARTES CONTRATANTES</h2><p><strong>LOCADORA:</strong></p><p>Nome da Empresa: {Nome da Empresa Locadora}<br>CNPJ: {CNPJ da Empresa Locadora}</p><p><strong>LOCATÁRIO:</strong></p><p>Nome: {Nome do Cliente}<br>CPF: {CPF do Cliente}</p><h2>OBJETO DO CONTRATO</h2><p>O presente contrato tem como objeto a locação do espaço denominado {Nome do Espaço}, localizado em {Bairro e Município do Espaço}, para a realização de evento conforme os detalhes abaixo.</p><h2>DETALHES DO EVENTO</h2><p>Data de Início: {Data de Início do Evento}<br>Hora de Início: {Hora de Início do Evento}</p><p>Data de Término: {Data de Término do Evento}<br>Hora de Término: {Hora de Término do Evento}</p><h2>DURAÇÃO DA LOCAÇÃO</h2><p>Total de horas locadas: {Número de Horas}</p><h2>VALOR E FORMA DE PAGAMENTO</h2><p>O LOCATÁRIO pagará à LOCADORA o valor de R\$ {Valor por Hora} por hora de utilização do espaço, totalizando R\$ {Valor Total das Horas}.</p><p>Será cobrada uma Taxa Concierge de 3,5% sobre o valor total da locação, correspondendo a R\$ {Valor da Taxa Concierge}.</p><p>Desconto aplicado: R\$ {Valor do Desconto} (Cupom: {Código do Cupom})</p><p><strong>VALOR TOTAL A PAGAR:</strong> R\$ {Valor Total a Pagar}</p><h2>REGRAS DE TAXAS E MULTAS</h2><p><strong>Entrega do Espaço:</strong> O horário de fim de uso é o momento da entrega do espaço. Caso o LOCATÁRIO exceda o tempo contratado, será cobrada uma multa de R\$ {Valor da Multa por Hora Extrapolada} por hora adicional ou fração de hora.</p><p><strong>Pagamento:</strong> O pagamento é efetuado integralmente ao final do processo de reserva, via Pix (que gera uma chave para pagamento de até 5 minutos) ou cartão de crédito.</p><p><strong>Cancelamento:</strong> Em caso de cancelamento por parte do LOCATÁRIO, será aplicada uma taxa de cancelamento de 20% até antes de 48 horas do evento ou 50% em menos de 48 horas antes do evento, não sendo possível cancelar a reserva em menos de 24 horas antes do evento.</p><h2>OBRIGAÇÕES DO LOCATÁRIO</h2><p><strong>Uso Adequado:</strong> Utilizar o espaço de forma adequada, respeitando as regras estabelecidas pela LOCADORA e as normas vigentes.</p><p><strong>Limpeza e Conservação:</strong> Manter o espaço limpo e conservado, sendo responsável por quaisquer danos causados durante o período de locação.</p><p><strong>Segurança:</strong> Responsabilizar-se pela segurança de seus convidados e pelo cumprimento das normas de segurança do espaço.</p><h2>DISPOSIÇÕES GERAIS</h2><p><strong>Alterações:</strong> Qualquer alteração no presente contrato deverá ser feita por escrito e assinada por ambas as partes.</p><p><strong>Jurídico:</strong> Para dirimir quaisquer controvérsias oriundas do presente contrato, as partes elegem o foro da comarca de {Cidade}, estado de {Estado}.</p><h2>ASSINATURA</h2><p>Por estarem de acordo com todas as cláusulas e condições estabelecidas neste contrato, as partes assinam este documento em duas vias de igual teor e forma, juntamente com duas testemunhas.</p><p>{Cidade}, {Data}</p><p><strong>Locadora:</strong><br>[Assinatura registrada do responsável pelo espaço]<br>Responsável pelo espaço: {Nome do responsável pelo espaço}<br>CPF: {CPF do responsável pelo espaço}</p><p><strong>Locatário:</strong><br>[Assinatura do cliente]<br>Responsável pela locação: {Nome do Cliente}<br>CPF: {CPF do Cliente}</p>',
+            data: widget.html,
             style: {
               'body': Style(
                 fontSize: FontSize(12.0),
