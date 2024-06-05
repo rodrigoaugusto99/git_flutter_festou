6431c0e544358a85cda4ff399ebeebc1478d3cb3
Maicon Moraes <maicon3000@hotmail.com>
2024-06-04 23:27:34 -0300

    FESTOU-159 - Ajustar tela Perfil

--- lib/src/features/bottomNavBar/bottomNavBarPage.dart
+++ lib/src/features/bottomNavBar/bottomNavBarPage.dart
@@ -1,8 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:git_flutter_festou/src/features/bottomNavBar/my%20favorite%20spaces%20mvvm/my_favorite_spaces_page.dart';
 import 'package:git_flutter_festou/src/features/bottomNavBar/profile/profile.dart';
 import 'package:git_flutter_festou/src/features/bottomNavBar/search/search_page.dart';
 import 'package:git_flutter_festou/src/features/bottomNavBar/home/home_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:git_flutter_festou/src/features/widgets/notifications_counter.dart';
+import 'package:stylish_bottom_bar/stylish_bottom_bar.dart';
 
 class BottomNavBarPage extends StatefulWidget {
   const BottomNavBarPage({super.key});
@@ -19,6 +24,7 @@ class _BottomNavBarPageState extends State<BottomNavBarPage> {
   void initState() {
     super.initState();
     _pageController = PageController(initialPage: _currentIndex);
+    NotificationCounter();
   }
 
   @override
@@ -41,13 +47,95 @@ class _BottomNavBarPageState extends State<BottomNavBarPage> {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        return false;
-      },
-      child: Scaffold(
-        extendBody: true,
-        body: PageView.builder(
+    return Scaffold(
+      extendBody: true,
+      bottomNavigationBar: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('users')
+            .where('uid', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+            .snapshots(),
+        builder: (context, snapshot) {
+          int notificationCount = 0;
+          if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
+            var userDoc = snapshot.data!.docs.first;
+            notificationCount = userDoc['notifications'] ?? 0;
+          }
+
+          return StylishBottomBar(
+            option: DotBarOptions(
+              dotStyle: DotStyle.tile,
+              gradient: const LinearGradient(
+                colors: [
+                  Colors.deepPurple,
+                  Colors.pink,
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            items: [
+              BottomBarItem(
+                icon: const Icon(
+                  Icons.house_outlined,
+                ),
+                selectedIcon: const Icon(Icons.house_rounded),
+                selectedColor: Colors.teal,
+                unSelectedColor: Colors.grey,
+                title: const Text('Início'),
+              ),
+              BottomBarItem(
+                icon: const Icon(Icons.search_outlined),
+                selectedIcon: const Icon(Icons.search),
+                selectedColor: Colors.blue,
+                title: const Text('Buscar'),
+              ),
+              BottomBarItem(
+                  icon: const Icon(
+                    CupertinoIcons.heart,
+                  ),
+                  selectedIcon: const Icon(
+                    CupertinoIcons.heart_fill,
+                  ),
+                  selectedColor: Colors.red,
+                  title: const Text('Favoritos')),
+              BottomBarItem(
+                icon: const Icon(
+                  Icons.person_outline,
+                ),
+                selectedIcon: const Icon(
+                  Icons.person,
+                ),
+                selectedColor: Colors.deepPurple,
+                title: const Text('Perfil'),
+                badge:
+                    Text(notificationCount > 9 ? '9+' : '$notificationCount'),
+                showBadge: notificationCount > 0,
+                badgeColor: Colors.purple,
+                badgePadding: const EdgeInsets.only(left: 4, right: 4),
+              ),
+            ],
+            hasNotch: true,
+            fabLocation: StylishBarFabLocation.end,
+            currentIndex: _currentIndex,
+            notchStyle: NotchStyle.square,
+            onTap: (index) {
+              if (index == _currentIndex) return;
+              _pageController.jumpToPage(index);
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+          );
+        },
+      ),
+      floatingActionButton: Image.asset(
+        'lib/assets/images/festou-logo.png',
+        scale: 5,
+        fit: BoxFit.cover,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      body: SafeArea(
+        child: PageView.builder(
           physics: const NeverScrollableScrollPhysics(),
           controller: _pageController,
           onPageChanged: _onPageChanged,
@@ -65,55 +153,6 @@ class _BottomNavBarPageState extends State<BottomNavBarPage> {
                 return Container(); // Lida com índices fora do alcance, se aplicável
             }
           },
-        ),
-        bottomNavigationBar: ClipRRect(
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(24.0),
-            topRight: Radius.circular(24.0),
-          ),
-          child: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            currentIndex: _currentIndex,
-            elevation: 4,
-            onTap: _onTabTapped,
-            selectedItemColor: const Color(0xff304571),
-            selectedLabelStyle: const TextStyle(
-              fontWeight: FontWeight.bold,
-            ),
-            unselectedItemColor: const Color(0xff304571),
-            items: [
-              BottomNavigationBarItem(
-                icon: Image.asset(
-                  'lib/assets/images/Ellipse 7casinha.png',
-                ),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Image.asset(
-                  'lib/assets/images/Ellipse 8lupinha.png',
-                ),
-                label: 'Search',
-              ),
-              BottomNavigationBarItem(
-                icon: Image.asset(
-                  'lib/assets/images/Ellipse 9documentozinho.png',
-                ),
-                label: 'Favorites',
-              ),
-              BottomNavigationBarItem(
-                icon: Image.asset(
-                  'lib/assets/images/Ellipse 10pessoinha.png',
-                ),
-                label: 'Profile',
-              ),
-            ],
-          ),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: Image.asset(
-          'lib/assets/images/festou-logo.png',
-          scale: 5,
-          fit: BoxFit.cover,
         ),
       ),
     );
