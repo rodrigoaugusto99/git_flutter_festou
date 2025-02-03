d7aa0d5eb91421d078aa9885dba1c1a2139f4eca
Maicon Moraes <maicon3000@hotmail.com>
2025-02-03 20:38:51 -0300

    FESTOU-222 - Adjusting icon size

--- lib/src/features/bottomNavBar/profile/pages/pagamentos/new_card_view.dart
+++ lib/src/features/bottomNavBar/profile/pages/pagamentos/new_card_view.dart
@@ -424,8 +424,8 @@ class _NewCardViewState extends State<NewCardView> {
                   children: [
                     CustomTextformfield(
                       onChanged: (p0) => setState(() {}),
-                      ddd: 9,
-                      scale: 2.5,
+                      ddd: 0,
+                      scale: 3.5,
                       svgPath: 'lib/assets/images/icon_pessoa.png',
                       hintText: 'Nome do portador',
                       controller: nameEC,
@@ -436,7 +436,7 @@ class _NewCardViewState extends State<NewCardView> {
                       onChanged: (p0) => setState(() {}),
                       ddd: 0,
                       svgWidth: 5,
-                      scale: 2.8,
+                      scale: 3.8,
                       horizontalPadding: 0,
                       hintText: 'Número do cartão',
                       keyboardType: TextInputType.number,
@@ -451,7 +451,8 @@ class _NewCardViewState extends State<NewCardView> {
                         Expanded(
                           child: CustomTextformfield(
                             onChanged: (p0) => setState(() {}),
-                            ddd: 12,
+                            ddd: 0,
+                            scale: 3.3,
                             svgPath: 'lib/assets/images/icon_card_cvv.png',
                             controller: cvvEC,
                             inputFormatters: [cvvFormatter],
@@ -464,7 +465,7 @@ class _NewCardViewState extends State<NewCardView> {
                         Expanded(
                           child: CustomTextformfield(
                             onChanged: (p0) => setState(() {}),
-                            ddd: 6,
+                            ddd: 0,
                             validator: (value) {
                               final unmaskedValue =
                                   value != null && value.isNotEmpty
@@ -476,7 +477,7 @@ class _NewCardViewState extends State<NewCardView> {
                               }
                               return null;
                             },
-                            scale: 2.3,
+                            scale: 3.3,
                             svgPath: 'lib/assets/images/icon_calendar.png',
                             keyboardType: TextInputType.number,
                             controller: validateDateEC,
@@ -489,7 +490,8 @@ class _NewCardViewState extends State<NewCardView> {
                     const SizedBox(height: 15),
                     CustomTextformfield(
                       onChanged: (p0) => setState(() {}),
-                      ddd: 12,
+                      ddd: 0,
+                      scale: 3.3,
                       validator: validate(),
                       svgPath: 'lib/assets/images/icon_card_check.png',
                       controller: cardNameEC,
