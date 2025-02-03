034ce3549126e764ed4a35808011d9fc14ae61d3
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2025-02-02 23:54:51 -0300

    fixing app bar, setting description max length with 200

--- lib/src/features/register/posts/register_post_page.dart
+++ lib/src/features/register/posts/register_post_page.dart
@@ -33,6 +33,7 @@ class _RegisterPostPageState extends State<RegisterPostPage> {
   }
 
   void onViewModelChanged() {
+    if (!mounted) return;
     setState(() {});
   }
 
@@ -42,212 +43,212 @@ class _RegisterPostPageState extends State<RegisterPostPage> {
 
     log(vm.photoShowing.toString());
     return Scaffold(
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          color: Colors.grey[200],
-          // gradient: LinearGradient(
-          //   colors: [
-          //     const Color(0xff9747FF).withOpacity(1),
-          //     const Color(0xff4300B1).withOpacity(1),
-          //   ],
-          //   begin: Alignment.topCenter,
-          //   end: Alignment.bottomCenter,
-          // ),
-          borderRadius: BorderRadius.circular(24),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Postar feed',
+          style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
         ),
-        padding: const EdgeInsets.all(30),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            //todo: opcao de capa de post (colocar no index 0 do array)
-            //todo:p isso, exibir miniaturas em listview hori. e cover icon por cima aonde clicar, sinalizand.
-            CustomTextformfield(
-              label: 'Nome',
-              controller: vm.tituloEC,
-              validator: vm.validate(),
-            ),
-            /*
-                  os textfields, ao serem clicados, abrira um dialog com o campo a ser
-                  preenchido e os botoes cancelar e salvar. Ao salvar, o texto vai aparecer 
-                  no espaco abaixo, aparecendo botao de editar. titulo obrigatorio apenas
-                   */
-            const SizedBox(height: 15),
-            CustomTextformfield(
-              label: 'Descrição',
-              controller: vm.descricaoEC,
-              validator: vm.validate(),
-            ),
-            const SizedBox(height: 20),
-            GestureDetector(
-              onTap: vm.register,
-              child: Container(
-                  alignment: Alignment.center,
-                  height: 35,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    gradient: const LinearGradient(
-                      colors: [
-                        Color(0xff9747FF),
-                        Color(0xff44300b1),
-                      ],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                    ),
-                  ),
-                  child: const Text(
-                    'Publicar',
-                    style: TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.w700,
-                        color: Colors.white),
-                  )),
-            ),
-          ],
-        ),
+        elevation: 0,
+        backgroundColor: const Color(0xfff8f8f8),
       ),
       backgroundColor: const Color(0xffF8F8F8),
-      appBar: AppBar(
-        title: const Text(''),
-      ),
-      body: Form(
-        key: vm.formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            //todo: quadrado grande com foto no meio
-            Padding(
-              padding: const EdgeInsets.only(top: 32, left: 32, right: 32),
-              child: decContainer(
-                onTap: vm.pickImages,
-                allPadding: 10,
-                height: MediaQuery.of(context).size.height / 3,
-                color: const Color(0xff9747FF).withOpacity(0.3),
-                radius: 10,
-                child: vm.photoShowing == null
-                    ? decContainer(
-                        color: const Color(0xffF2F2F2),
-                        radius: 25,
-                        child: Stack(
-                          alignment: Alignment.center,
+      body: SingleChildScrollView(
+        child: Form(
+          key: vm.formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              //todo: quadrado grande com foto no meio
+              Padding(
+                padding: const EdgeInsets.only(top: 32, left: 32, right: 32),
+                child: decContainer(
+                  onTap: vm.pickImages,
+                  allPadding: 10,
+                  height: MediaQuery.of(context).size.height / 3,
+                  color: const Color(0xff9747FF).withOpacity(0.3),
+                  radius: 10,
+                  child: vm.photoShowing == null
+                      ? decContainer(
+                          color: const Color(0xffF2F2F2),
+                          radius: 25,
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              decContainer(
+                                height: 90,
+                                width: 90,
+                                radius: 100,
+                                color: Colors.white,
+                                allPadding: 10,
+                                child: SvgPicture.asset(
+                                  'lib/assets/images/icon_camera.svg',
+                                  color: const Color(0xff4300B1),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      : Stack(
                           children: [
-                            decContainer(
-                              height: 90,
-                              width: 90,
-                              radius: 100,
-                              color: Colors.white,
-                              allPadding: 10,
-                              child: SvgPicture.asset(
-                                'lib/assets/images/icon_camera.svg',
-                                color: const Color(0xff4300B1),
+                            Image.file(
+                              vm.photoShowing!,
+                              fit: BoxFit.cover,
+                            ),
+                            Positioned(
+                              bottom: 10,
+                              right: 0,
+                              child: decContainer(
+                                height: 90,
+                                width: 90,
+                                radius: 100,
+                                color: Colors.white.withOpacity(0.6),
+                                allPadding: 10,
+                                child: SvgPicture.asset(
+                                  'lib/assets/images/icon_camera.svg',
+                                  color: const Color(0xff4300B1),
+                                ),
                               ),
                             ),
                           ],
                         ),
-                      )
-                    : Stack(
-                        children: [
-                          Image.file(
-                            vm.photoShowing!,
-                            fit: BoxFit.cover,
-                          ),
-                          Positioned(
-                            bottom: 10,
-                            right: 0,
-                            child: decContainer(
-                              height: 90,
-                              width: 90,
-                              radius: 100,
-                              color: Colors.white.withOpacity(0.6),
-                              allPadding: 10,
-                              child: SvgPicture.asset(
-                                'lib/assets/images/icon_camera.svg',
-                                color: const Color(0xff4300B1),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            if (vm.imagesToRegister.isNotEmpty)
-              const Padding(
-                padding: EdgeInsets.only(left: 32),
-                child: Text('Escolha sua foto de capa'),
-              ),
-            const SizedBox(
-              height: 10,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 32),
-              child: SizedBox(
-                height: 100,
-                child: ListView.builder(
-                  clipBehavior: Clip.none,
-                  scrollDirection: Axis.horizontal,
-                  itemCount: vm.imagesToRegister.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 5),
-                      child: Stack(
-                        clipBehavior: Clip.none,
-                        children: [
-                          decContainer(
-                            onTap: () => vm.onPhotoTap(index),
-                            height: 100,
-                            width: 100,
-                            radius: 10,
-                            color: Colors.grey[400],
-                            allPadding: 7,
-                            child: decContainer(
-                              radius: 10,
-                              color: Colors.red,
-                              child: Image.file(
-                                vm.imagesToRegister[index],
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                          if (vm.photoShowing == vm.imagesToRegister[index])
-                            Stack(
-                              alignment: Alignment.center,
-                              children: [
-                                const Icon(
-                                  Icons.photo,
-                                  size: 50,
-                                  color: Colors.black,
-                                ),
-                                decContainer(
-                                  radius: 10,
-                                  color: Colors.black.withOpacity(0.5),
-                                  height: 100,
-                                  width: 100,
-                                ),
-                              ],
-                            ),
-                          Positioned(
-                            top: -10,
-                            right: -10,
-                            child: decContainer(
-                              onTap: () => vm.deletarImagem(index),
-                              radius: 50,
-                              color: Colors.red,
-                              child: const Icon(
-                                Icons.close,
-                                color: Colors.white,
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    );
-                  },
                 ),
               ),
-            )
-          ],
+              const SizedBox(
+                height: 10,
+              ),
+              if (vm.imagesToRegister.isNotEmpty)
+                const Padding(
+                  padding: EdgeInsets.only(left: 32),
+                  child: Text('Escolha sua foto de capa'),
+                ),
+              const SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 32),
+                child: SizedBox(
+                  height: 100,
+                  child: ListView.builder(
+                    clipBehavior: Clip.none,
+                    scrollDirection: Axis.horizontal,
+                    itemCount: vm.imagesToRegister.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 5),
+                        child: Stack(
+                          clipBehavior: Clip.none,
+                          children: [
+                            decContainer(
+                              onTap: () => vm.onPhotoTap(index),
+                              height: 100,
+                              width: 100,
+                              radius: 10,
+                              color: Colors.grey[400],
+                              allPadding: 7,
+                              child: decContainer(
+                                radius: 10,
+                                color: Colors.red,
+                                child: Image.file(
+                                  vm.imagesToRegister[index],
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            if (vm.photoShowing == vm.imagesToRegister[index])
+                              Stack(
+                                alignment: Alignment.center,
+                                children: [
+                                  const Icon(
+                                    Icons.photo,
+                                    size: 50,
+                                    color: Colors.black,
+                                  ),
+                                  decContainer(
+                                    radius: 10,
+                                    color: Colors.black.withOpacity(0.5),
+                                    height: 100,
+                                    width: 100,
+                                  ),
+                                ],
+                              ),
+                            Positioned(
+                              top: -10,
+                              right: -10,
+                              child: decContainer(
+                                onTap: () => vm.deletarImagem(index),
+                                radius: 50,
+                                color: Colors.red,
+                                child: const Icon(
+                                  Icons.close,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    //todo: opcao de capa de post (colocar no index 0 do array)
+                    //todo:p isso, exibir miniaturas em listview hori. e cover icon por cima aonde clicar, sinalizand.
+                    CustomTextformfield(
+                      label: 'Nome',
+                      controller: vm.tituloEC,
+                      validator: vm.validate(),
+                    ),
+                    /*
+                      os textfields, ao serem clicados, abrira um dialog com o campo a ser
+                      preenchido e os botoes cancelar e salvar. Ao salvar, o texto vai aparecer 
+                      no espaco abaixo, aparecendo botao de editar. titulo obrigatorio apenas
+                       */
+                    const SizedBox(height: 15),
+                    CustomTextformfield(
+                      label: 'Descrição',
+                      controller: vm.descricaoEC,
+                      validator: vm.validate(),
+                      isBig: true,
+                      maxLength: 200,
+                    ),
+                    const SizedBox(height: 20),
+                    GestureDetector(
+                      onTap: vm.register,
+                      child: Container(
+                        alignment: Alignment.center,
+                        height: 35,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          gradient: const LinearGradient(
+                            colors: [
+                              Color(0xff9747FF),
+                              Color(0xff44300b1),
+                            ],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                          ),
+                        ),
+                        child: const Text(
+                          'Publicar',
+                          style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/src/features/widgets/custom_textformfield.dart
+++ lib/src/features/widgets/custom_textformfield.dart
@@ -26,9 +26,11 @@ class CustomTextformfield extends StatefulWidget {
   Widget? prefixIcon;
   bool? withCrazyPadding;
   Key? customKey;
+  int? maxLength;
   CustomTextformfield({
     super.key,
     this.label,
+    this.maxLength,
     this.customKey,
     this.hintText,
     this.prefixIcon,
@@ -60,6 +62,25 @@ class CustomTextformfield extends StatefulWidget {
 bool isVisible = false;
 
 class _CustomTextformfieldState extends State<CustomTextformfield> {
+//  final TextEditingController _controller = TextEditingController();
+  int _charCount = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.controller.addListener(() {
+      setState(() {
+        _charCount = widget.controller.text.length;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    widget.controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -76,17 +97,20 @@ class _CustomTextformfieldState extends State<CustomTextformfield> {
             : isVisible
                 ? false
                 : true,
-        onChanged: widget.onChanged,
+        onChanged: widget.onChanged ?? (v) {},
         enabled: widget.enable,
         controller: widget.controller,
         validator: widget.validator,
-        keyboardType: widget.keyboardType,
-        onTapOutside: (event) => {FocusScope.of(context).unfocus()},
+        keyboardType: widget.keyboardType, maxLength: widget.maxLength,
+        // onTapOutside: (event) => {FocusScope.of(context).unfocus()},
         style: TextStyle(
             fontSize: 14,
             color: widget.enable ? Colors.black : Colors.grey,
             overflow: TextOverflow.ellipsis),
         decoration: InputDecoration(
+          counterText: widget.maxLength == null
+              ? null
+              : "$_charCount / ${widget.maxLength}",
           hintText: widget.hintText,
           prefixIcon: widget.svgPath != null
               ? Padding(
